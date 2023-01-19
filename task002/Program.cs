9be7aa032be558af3b958e35584004d109e7a6ae
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-01-19 23:24:06 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,25 @@
+﻿int numberA, numberB;
+System.Console.Write("Введите первое число: ");
+while(!Int32.TryParse(Console.ReadLine(), out numberA))
+{
+    Console.Write("Ошибка! Введите цифру: ");
+}
+System.Console.Write("Введите второе число: ");
+while(!Int32.TryParse(Console.ReadLine(), out numberB))
+{
+    Console.Write("Ошибка! Введите цифру: ");
+}
+if(numberA > numberB)
+{
+    System.Console.WriteLine($"min = {numberB}");
+    System.Console.WriteLine($"max = {numberA}");
+}
+else if(numberB > numberA)
+{
+    System.Console.WriteLine($"min = {numberA}");
+    System.Console.WriteLine($"max = {numberB}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
