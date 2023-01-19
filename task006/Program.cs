38790385107c18ec6ee42524fc3e07c2652f89f4
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-01-19 23:47:50 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,14 @@
+﻿int number;
+System.Console.Write("Введите целое число: ");
+while(!Int32.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.Write("Некорректное число! Введите целое число: ");
+}
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("Чётное");
+}
+else
+{
+    System.Console.WriteLine("Нечётное");
+}
