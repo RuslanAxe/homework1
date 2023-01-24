6034f700790b3655bd620ec795390d6ad1bb3ab4
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-01-24 15:34:39 +0300

    task010-13-15

---
+++ task010/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+double number;
+System.Console.Write("Введите трёхзначное число: ");
+while(!double.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.Write("Введено не число! Введите число: ");
+}
+number = Math.Abs(number);
+
+if(((int)number / 100 > 0) && ((int)number / 1000 == 0))
+{
+SecondDigit(number);
+}
+System.Console.WriteLine(result);
+else
+{
+System.Console.WriteLine("Не верный ввод числа!");
+}
+System.Console.WriteLine();    
+static void SecondDigit(double number);
+{ int result;
+if(number > 99 && number < 1000)
+    {
+        number = (int)number / 10;
+        result = (int)number % 10;
+        System.Console.WriteLine($"Вторая цифра: {result}");
+    }
+else 
+    {
+        System.Console.WriteLine("Число не трёхзначное!!");
+    }
+}
+
+
---
+++ task013/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+double number;
+System.Console.Write("Введите число трехзначное: ");
+while(!double.TryParse(Console.ReadLine(), out number))
+{
+    System.Console.Write("Неправильный ввод данных! Введите число: ");
+}
+ThirdDigit(number);                    
+System.Console.WriteLine();
+
+static void ThirdDigit(double number)
+{
+int result;
+number = Math.Abs(number);
+if (number > 99)
+    {
+    while (number > 999)
+        {
+        number /= 10;
+        }
+    result = (int)number % 10;
+    System.Console.WriteLine($"Третья цифра в числе: {result}");
+    }
+else
+    {
+    System.Console.WriteLine("Третьей цифры нет");
+    }
+}
