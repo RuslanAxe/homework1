de41251907b745acd6faa8b16252f3dbcc03023a
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-02-10 20:11:49 +0300

    решил задачу 029

--- task029/Program.cs
+++ task029/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, -5, 0, -11 -> [1, 2, 5, 7, 19, -5, 0, -11]
+
+void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random() .Next();
+        index++;
+    }
+
+}
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{(coll[position])}" + ",");
+        position++;
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
